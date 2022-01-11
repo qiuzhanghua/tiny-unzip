ea92f60e35ba216c4edebadb011ea1031d8b26a3
qiuzhanghua <qiuzhanghua@icloud.com>
2022-01-11 09:00:52 +0800

    -> 1.2.1

--- autotag.go
+++ autotag.go
@@ -1,5 +1,5 @@
 package main
 
-var AppVersion = "v1.1.0"
-var AppRevision = "a258bcc"
-var AppBuildDate = "2021-12-27"
+var AppVersion = "v1.2.1"
+var AppRevision = "5324194"
+var AppBuildDate = "2022-01-11"
